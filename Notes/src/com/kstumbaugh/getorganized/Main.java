dfc194de304e5bec69c5d72b1fe1bc80d3948d2c
Ken Stumbaugh <stumbaugh.k1@gmail.com>
2020-04-26 14:34:15 -0400

    Added Data Type notes to new file

--- Notes/src/com/kstumbaugh/getorganized/Main.java
+++ Notes/src/com/kstumbaugh/getorganized/Main.java
@@ -1,8 +1,40 @@
 package com.kstumbaugh.getorganized;
 
 public class Main {
+    //TODO: literally just notes
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  	/*
+	No rounding in Basic Math operators
+	13/5 = 2 and not 3 due to rounding
+
+    ++i = increments by 1 before returning value
+	i++ = increments by 1 after returning value
+
+    --i = decrements by 1 before returning value
+	i-- = decrements by 1 after returning value
+	*/
+
+    /* Order of operation:*/
+    //Postfix i++ i--
+    //Prefix --i ++i
+    //Multi / * %
+    //Add + -
+
+    /*Data Type conversion*/
+    //implicit - widening (wider data type = more space for value)
+    //Uses largest available size for data set
+    //mix int&float uses largest float in equation
+    int iVal = 50;
+    long lVal = iVal;
+    //explicit - widening or narrowing, use caution
+    //floating to integer drops fraction
+    //integer to floating can lose precision
+    long lVal2 = 50;
+    int iVal2 = (int) lVal2;
+
+    /*
+    * This is going to be a lot of notes for the project that I am trying to do
+    Testing out GitHub again
+    * Keeping up to date with what I need to do in the Java course
+    */
 }
