4e7611b1e1d4c26c109829702ef35534c5c9d33e
Ken Stumbaugh <stumbaugh.k1@gmail.com>
2020-04-26 14:58:15 -0400

    Demo additional

--- DataTypeDemo/src/com/kstumbaugh/typeconversion/Main.java
+++ DataTypeDemo/src/com/kstumbaugh/typeconversion/Main.java
@@ -11,6 +11,14 @@ public class Main {
         long longVal = 5;
 
         short result1 = byteVal;
+        //Not valid: short resultsl = longVal;
+        short resultsl1 = (short) longVal;
+
+        //Not valid(long value in equation): short result2 = byteVal - longVal;
+        short result2 = (short) (byteVal - longVal); //explicit cast to short
+
+        //Not valid(floating result): long result3 = longVal - floatVal;
+        float result3 = longVal - floatVal; //long can convert to float
 
          System.out.println("Success!");
 
